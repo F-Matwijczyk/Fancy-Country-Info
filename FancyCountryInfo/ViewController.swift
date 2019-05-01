a4fe07f9e1bca23b46e3c1f926730df616342034
Jimmy <jimmy.phoaq@gmal.com>
2019-05-01 20:58:55 +0200

    Initial Commit

---
+++ FancyCountryInfo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FancyCountryInfo
+//
+//  Created by jimmy on 01/05/2019.
+//  Copyright © 2019 jimmy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
